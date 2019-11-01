6fb9e5c8e057c4b6c174dcd3deef301bd918f506
alvinl <alvinl@synnex.com>
2019-11-01 16:25:19 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DataStructures-Algorithms
-The repository is used to note data structure and algorithms demo
+The repository is used to note data structure and algorithms sortAlgorithmsDemo
+
+# 排序算法
+## 1 计数排序
+## 2 桶排序
---
+++ src/main/java/com/alvin/algorithms/SortAlgorithmsDemo.java
@@ -0,0 +1,55 @@
+package com.alvin.algorithms;
+
+import java.util.Arrays;
+
+/**
+ * <p>
+ * TODO
+ * </p>
+ *
+ * @author alvinl
+ * @date 11/01/19 10:10
+ **/
+public class SortAlgorithmsDemo {
+    int[] arr = {8, 3, 9, 1, 5, 2, 7};
+    int size = arr.length;
+
+    public void testBubbleSort() {
+
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                int temp;
+                if (arr[i] > arr[j]) {
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public void testInsertionSort() {
+        for (int i = 0; i < size; i++) {
+            int temp = arr[i];
+
+            int leftIndex = i - 1;
+            while (leftIndex >= 0 && arr[leftIndex] > temp) {
+                arr[leftIndex + 1] = arr[leftIndex];
+                leftIndex--;
+            }
+            arr[leftIndex+1] = temp;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+
+    public static void main(String[] args) {
+        SortAlgorithmsDemo sortAlgorithmsDemo = new SortAlgorithmsDemo();
+        sortAlgorithmsDemo.testBubbleSort();
+        sortAlgorithmsDemo.testInsertionSort();
+    }
+}
